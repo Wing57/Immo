9d158004b3726f571d1d3ea58034bd5e1a2c5512
WIng57 <ddariusd007@gmail.com>
2022-12-05 00:06:22 -0500

    more bs on drivetrain but its clean

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,49 +4,48 @@
 
 package frc.robot.subsystems;
 
-import java.util.Arrays;
-import java.util.List;
-
 import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.Settings;
+import java.util.Arrays;
+import java.util.List;
 
 public class DriveTrain extends SubsystemBase {
 
   private final List<CANSparkMax> allMotors;
 
-  private final CANSparkMax leftMaster;
-  private final CANSparkMax leftMotor2;
-  private final CANSparkMax leftMotor3;
+  private final CANSparkMax leftMaster, leftMotor2, leftMotor3;
+  private final CANSparkMax rightMaster, rightMotor2, rightMotor3;
 
-  private final CANSparkMax rightMaster;
-  private final CANSparkMax rightMotor2;
-  private final CANSparkMax rightMotor3;
-  
-  private final RelativeEncoder leftEncoder;
-  private final RelativeEncoder rightEncoder;
+  private final CANSparkMax[] leftMotors;
+  private final CANSparkMax[] rightMotors;
 
-  private final boolean rightMotorInvert;
-  private final boolean leftMotorInvert;
+  private final RelativeEncoder leftEncoder, rightEncoder;
+  private final boolean rightMotorInvert, leftMotorInvert;
 
   private final int currentLimit;
   private final double rampRate;
 
   private final DifferentialDrive drive;
-
   private final DifferentialDriveOdometry m_Odometry;
 
   private final Field2d field;
@@ -54,6 +53,7 @@ public class DriveTrain extends SubsystemBase {
   private final AHRS navX;
 
   public DriveTrain() {
+    // NEOs
     leftMaster = new CANSparkMax(DriveConstants.LEFT_MOTOR_1, MotorType.kBrushless);
     leftMotor2 = new CANSparkMax(DriveConstants.LEFT_MOTOR_2, MotorType.kBrushless);
     leftMotor3 = new CANSparkMax(DriveConstants.LEFT_MOTOR_3, MotorType.kBrushless);
@@ -62,13 +62,19 @@ public class DriveTrain extends SubsystemBase {
     rightMotor2 = new CANSparkMax(DriveConstants.LEFT_MOTOR_2, MotorType.kBrushless);
     rightMotor3 = new CANSparkMax(DriveConstants.LEFT_MOTOR_3, MotorType.kBrushless);
 
-    //TODO: test ramprate
+    // Motor groups
+    leftMotors = new CANSparkMax[] {leftMaster, leftMotor2, leftMotor3};
+
+    rightMotors = new CANSparkMax[] {rightMaster, rightMotor2, rightMotor3};
+
+    // TODO: test ramprate
     currentLimit = 40;
     rampRate = 0.25;
 
+    // Universal Motor Config
     allMotors =
-      Arrays.asList(leftMaster, leftMotor2, leftMotor3, rightMaster, rightMotor2, rightMotor3);
-    
+        Arrays.asList(leftMaster, leftMotor2, leftMotor3, rightMaster, rightMotor2, rightMotor3);
+
     allMotors.forEach(
         motor -> {
 
@@ -83,10 +89,11 @@ public class DriveTrain extends SubsystemBase {
           // (Values need testing)
           motor.setClosedLoopRampRate(rampRate);
 
-        }
-    );
-    
-    //TODO: Verify inversions
+          // Sets Motor to Brake/Coast
+          motor.setIdleMode(IdleMode.kBrake);
+        });
+
+    // TODO: Verify inversions
     leftMotorInvert = true;
     rightMotorInvert = false;
 
@@ -95,17 +102,24 @@ public class DriveTrain extends SubsystemBase {
 
     leftMotor2.follow(leftMaster, leftMotorInvert);
     leftMotor3.follow(leftMaster, leftMotorInvert);
-    
+
     rightMotor2.follow(rightMaster, rightMotorInvert);
     rightMotor3.follow(rightMaster, rightMotorInvert);
 
-    drive = new DifferentialDrive(leftMaster, rightMaster);
+    // DiffDrive jumbalaya
+    drive = new DifferentialDrive(leftMotors[0], rightMotors[0]);
 
+    // Encoder configs
     leftEncoder = leftMaster.getEncoder();
     rightEncoder = rightMaster.getEncoder();
 
+    leftEncoder.setInverted(leftMotorInvert);
+    rightEncoder.setInverted(rightMotorInvert);
+
+    // navX init
     navX = new AHRS(SPI.Port.kMXP);
 
+    // Odometry init
     m_Odometry = new DifferentialDriveOdometry(navX.getRotation2d());
     field = new Field2d();
     resetOdometry(Settings.STARTING_POSITION);
@@ -115,10 +129,55 @@ public class DriveTrain extends SubsystemBase {
   }
 
   // *****************************************
+  // ************** Driving ******************
+  // *****************************************
+
+  public void tankDrive(double left, double right) {
+    drive.tankDrive(left, right);
+    drive.feed();
+  }
+
+  public void arcadeDrive(double xSpeed, double zRotation) {
+    drive.arcadeDrive(xSpeed, zRotation, false);
+    drive.feed();
+  }
+
+  public void curvatureDrive(double xSpeed, double zRotation, double turnSpeed) {
+    // Clamp all inputs to valid values
+    xSpeed = MathUtil.clamp(xSpeed, -1.0, 1.0);
+    zRotation = MathUtil.clamp(zRotation, -1.0, 1.0);
+    turnSpeed = MathUtil.clamp(turnSpeed, 0.0, 1.0);
+
+    // Change turn speed based off of robot speed
+    double turnAdj = Math.max(turnSpeed, Math.abs(xSpeed));
+
+    // Find the speeds of the left and right wheels
+    double leftSpeed = xSpeed + zRotation * turnAdj;
+    double rightSpeed = xSpeed - zRotation * turnAdj;
+
+    // If a wheel goes above 1.0, scale down the other wheels to compensate
+    double scale = Math.max(1.0, Math.max(Math.abs(leftSpeed), Math.abs(rightSpeed)));
+
+    leftSpeed /= scale;
+    rightSpeed /= scale;
+
+    // Feed the inputs to the drivetrain
+    tankDrive(leftSpeed, rightSpeed);
+  }
+
+  public void curvatureDrive(double xSpeed, double zRotation, boolean stationaryTurn) {
+    drive.curvatureDrive(xSpeed, zRotation, stationaryTurn);
+  }
+
+  public void stopMotors() {
+    drive.stopMotor();
+  }
+
+  // *****************************************
   // ************** Encoders *****************
   // *****************************************
 
-  //TODO: Figure out if native unit conversion is needed
+  // TODO: Figure out if native unit conversion is needed
   public double getLeftDistance() {
     return leftEncoder.getPosition();
   }
@@ -158,7 +217,7 @@ public class DriveTrain extends SubsystemBase {
     return navX.getAngle();
   }
 
-  //TODO: test that the angle stuff checks out
+  // TODO: test that the angle stuff checks out
   public double getGyroAngle() {
     return navX.getAngle() % 360;
   }
@@ -180,18 +239,11 @@ public class DriveTrain extends SubsystemBase {
   // ********************************************
 
   public void updateOdometry() {
-    m_Odometry.update(
-      getRotation2d(), 
-      getLeftDistance(), 
-      getRightDistance()
-    );
+    m_Odometry.update(getRotation2d(), getLeftDistance(), getRightDistance());
   }
 
   public DifferentialDriveWheelSpeeds getWheelSpeeds() {
-    return new DifferentialDriveWheelSpeeds(
-      getLeftVelocity(), 
-      getRightVelocity()
-      );
+    return new DifferentialDriveWheelSpeeds(getLeftVelocity(), getRightVelocity());
   }
 
   public Pose2d getPose() {
@@ -214,9 +266,40 @@ public class DriveTrain extends SubsystemBase {
     resetOdometry(getPose());
   }
 
+  // *****************************************
+  // ************** Voltage ******************
+  // *****************************************
+
+  public double getBatteryVoltage() {
+    return RobotController.getBatteryVoltage();
+  }
+
+  public double getLeftVoltage() {
+    return leftMotors[0].get() * getBatteryVoltage();
+  }
+
+  public double getRightVoltage() {
+    return rightMotors[0].get() * getBatteryVoltage();
+  }
+
+  public void tankDriveVolts(double leftVolts, double rightVolts) {
+    for (MotorController motor : leftMotors) {
+      motor.setVoltage(leftVolts);
+    }
+
+    for (MotorController motor : rightMotors) {
+      motor.setVoltage(rightVolts);
+    }
+
+    drive.feed();
+  }
+
   @Override
   public void periodic() {
     updateOdometry();
     field.setRobotPose(getPose());
   }
+
+  @Override
+  public void initSendable(SendableBuilder builder) {}
 }
